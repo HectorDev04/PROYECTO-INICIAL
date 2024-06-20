a1f7ee8219d1392ef7ab7027a955158908a240f6
HectorDev04 <hectorrodriguezzzz04@gmail.com>
2024-06-20 04:58:03 -0600

    In process

---
+++ DESAFIOS/desafios2.js
@@ -0,0 +1,22 @@
+// DESAFIO 1
+let contador = 0;
+while (contador != 10) {
+    contador++
+    console.log(contador);
+}
+
+// DESAFIO 2
+let contador2 = 10
+while (contador2 != 0) {
+    contador2--
+    console.log(contador2);
+}
+
+// DESAFIO 3
+let contador3 = 0;
+let numLimit = prompt('Ingrese un número');
+while (contador3 != numLimit) {
+    contador3++
+    console.log(contador3);
+}
+
--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 //Variables
 let numSecret = 5;
 let numUser = 0;
-let intentos = 0;
+let intentos = 1;
+let maxIntentos = 3;
 
 while (numUser != numSecret) {
+    // REALIZAMOS LA COMPARACION
     numUser = prompt("Ingrese un numero entre 1 y 10 por favor: ");
     if (numUser == numSecret) {
-        alert(`Acertaste, el número es: ${numUser}`);
+        alert(`Acertaste, el número es ${numUser} Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
     } else if (numUser > numSecret) {
         alert('El número secreto es menor');
     } else {
         alert('El número secreto es mayor');
     };
-    intentos = intentos + 1
-    alert(`Intento: ${intentos}`)
+    // INCREMENTAMOS EL CONTADOR CUANDO NO ACIERTA
+    intentos++
+    if (intentos > maxIntentos) {
+        alert('Llegaste al limite de intentos')
+        break;
+    };
 };
