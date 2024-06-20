730ed87356b2da7b49e0480b02d64c36dfe7e8c5
HectorDev04 <hectorrodriguezzzz04@gmail.com>
2024-06-20 03:34:40 -0600

    In process

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+//Variables
 let numSecret = 5;
 let numUser = 0;
+let intentos = 0;
+
 while (numUser != numSecret) {
     numUser = prompt("Ingrese un numero entre 1 y 10 por favor: ");
     if (numUser == numSecret) {
@@ -7,6 +10,8 @@ while (numUser != numSecret) {
     } else if (numUser > numSecret) {
         alert('El número secreto es menor');
     } else {
-        alert('El número secreto es mayor')
+        alert('El número secreto es mayor');
     };
-}
+    intentos = intentos + 1
+    alert(`Intento: ${intentos}`)
+};
