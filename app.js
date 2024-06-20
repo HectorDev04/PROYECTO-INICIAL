dfedd714931e076bd1ef7d932f007e4b107ee143
HectorDev04 <hectorrodriguezzzz04@gmail.com>
2024-06-20 03:34:29 -0600

    In process

--- app.js
+++ app.js
@@ -1,52 +1,12 @@
-//Variables
-let numSecret = 1;
-let numUser = prompt("Ingrese un numero entre 1 y 10 por favor: ");
-let contador = 0;
-console.log(numUser);
-
-//Realizamos la comparacion
-if (numUser == numSecret) {
-    // La condicion se cumplio
-    contador = contador + 1;
-    alert(`Acertaste, el número es: ${numUser}, su puntaje actual es: ${contador}`);
-} else {
-    // La condicion no se cumplio
-    alert(`Lo siento no acertaste el número, su puntaje actual es: ${contador}`);
-
-};
-numUser = prompt("Ingrese un numero entre 1 y 10 por favor: ")
-numSecret = 2;
-if (numUser == numSecret) {
-    contador = contador + 1;
-    alert(`Acertaste, el número es: ${numUser}, su puntaje actual es: ${contador}`);
-} else {
-    alert(`Lo siento no acertaste el número, su puntaje actual es: ${contador}`);
-
-};
-numUser = prompt("Ingrese un numero entre 1 y 10 por favor: ")
-numSecret = 3;
-if (numUser == numSecret) {
-    contador = contador + 1;
-    alert(`Acertaste, el número es: ${numUser}, su puntaje actual es: ${contador}`);
-} else {
-    alert(`Lo siento no acertaste el número, su puntaje actual es: ${contador}`);
-
-};
-numUser = prompt("Ingrese un numero entre 1 y 10 por favor: ")
-numSecret = 4;
-if (numUser == numSecret) {
-    contador = contador + 1;
-    alert(`Acertaste, el número es: ${numUser}, su puntaje actual es: ${contador}`);
-} else {
-    alert(`Lo siento no acertaste el número, su puntaje actual es: ${contador}`);
-
-};
-numUser = prompt("Ingrese un numero entre 1 y 10 por favor: ")
-numSecret = 5;
-if (numUser == numSecret) {
-    contador = contador + 1;
-    alert(`Acertaste, el número es: ${numUser}, su puntaje actual es: ${contador}`);
-} else {
-    alert(`Lo siento no acertaste el número, su puntaje actual es: ${contador}`);
-
-};
+let numSecret = 5;
+let numUser = 0;
+while (numUser != numSecret) {
+    numUser = prompt("Ingrese un numero entre 1 y 10 por favor: ");
+    if (numUser == numSecret) {
+        alert(`Acertaste, el número es: ${numUser}`);
+    } else if (numUser > numSecret) {
+        alert('El número secreto es menor');
+    } else {
+        alert('El número secreto es mayor')
+    };
+}
